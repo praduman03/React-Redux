7fb810873132eb76b4edc9d2d671350a834231b0
praduman03 <113344907+praduman03@users.noreply.github.com>
2023-01-08 23:10:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LikeCounter from "./components/LikeCounter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LikeCounter />
+    </div>
+  );
+}
+
+export default App;
